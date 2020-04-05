6eabe1d07c46139807216aa5e410321625dc7e66
TimofeyZykov <zikovtm@gmail.com>
2020-04-05 19:42:42 +0300

    fix: add solutions for first and second ex.

--- RSSchool_T3/RSSchool_T3/1/PolynomialConverter.m
+++ RSSchool_T3/RSSchool_T3/1/PolynomialConverter.m
@@ -2,6 +2,31 @@
 
 @implementation PolynomialConverter
 - (NSString*)convertToStringFrom:(NSArray <NSNumber*>*)numbers {
-    return @"";
+    NSMutableString *result = [[NSMutableString alloc] init];
+    if (numbers.count == 0)
+        return nil;
+    for (int i = 0; i < numbers.count; i++) {
+        NSInteger value = [numbers[i] intValue];
+        if (!value)
+            continue;
+        NSString *sign = value < 0 ? @"-" : @"+";
+        NSInteger coeff = numbers.count-1 - i;
+        value = labs(value);
+        if (value == 1 && coeff == 1) {
+        [result appendString:[NSString stringWithFormat:@" %@ x", sign]];
+        } else if (coeff == 1) {
+            [result appendString:[NSString stringWithFormat:@" %@ %ldx", sign, value]];
+        } else if (coeff == 0) {
+            [result appendString:[NSString stringWithFormat:@" %@ %ld", sign, value]];
+        } else {
+            [result appendString:[NSString stringWithFormat:@" %@ %ldx^%ld", sign, value, coeff]];
+        }
+    }
+    if ([result hasPrefix:@" + "]) {
+        result = [result substringFromIndex:3];
+    } else {
+        [result replaceCharactersInRange:NSMakeRange(0, 3) withString:@"-"];
+    }
+    return result;
 }
 @end
--- RSSchool_T3/RSSchool_T3/2/Combinator.m
+++ RSSchool_T3/RSSchool_T3/2/Combinator.m
@@ -2,6 +2,15 @@
 
 @implementation Combinator
 - (NSNumber*)chechChooseFromArray:(NSArray <NSNumber*>*)array {
-    return @0;
+    NSInteger m = [array[0] intValue];
+    NSInteger n = [array[1] intValue];
+    
+    for (int i = 1; i < n; i++) {
+        if (m == (tgamma(n+1) / (tgamma(i+1) * tgamma((n - i)+1)))) {
+            return [NSNumber numberWithInt:i];
+        }
+    }
+    
+    return nil;
 }
 @end
